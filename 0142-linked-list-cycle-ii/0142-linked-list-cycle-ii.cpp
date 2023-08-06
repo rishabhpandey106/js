d2d79c3de002968006b9a2937099e47d50e851a3
Rishabh Kumar Pandey <118062111+rishabhpandey106@users.noreply.github.com>
2023-08-06 20:47:12 +0530

    Time: 2 ms (99.35%), Space: 7.7 MB (43.30%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,35 +10,35 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) 
     {
-        // ListNode *fast = head;
-        // ListNode *slow = head;
-        // do
-        // {
-        //     if(fast == NULL || fast->next == NULL)
-        //     return NULL;
-        //     fast = fast->next->next;
-        //     slow = slow->next;
-        // }while(fast != slow);
-        // slow = head;
-        // while(fast != slow)
-        // {
-        //     fast = fast->next;
-        //     slow = slow->next;
-        // }
-        // return slow;
-        
-        unordered_set<ListNode*> st;
-        ListNode* i = head;
-
-        while (i != NULL) 
+        ListNode *fast = head;
+        ListNode *slow = head;
+        do
         {
-            if (st.find(i) != st.end()) 
-            {
-                break;
-            }
-            st.insert(i);
-            i = i->next;
+            if(fast == NULL || fast->next == NULL)
+            return NULL;
+            fast = fast->next->next;
+            slow = slow->next;
+        }while(fast != slow);
+        slow = head;
+        while(fast != slow)
+        {
+            fast = fast->next;
+            slow = slow->next;
         }
-        return i;
+        return slow;
+        
+//         unordered_set<ListNode*> st;
+//         ListNode* i = head;
+
+//         while (i != NULL) 
+//         {
+//             if (st.find(i) != st.end()) 
+//             {
+//                 break;
+//             }
+//             st.insert(i);
+//             i = i->next;
+//         }
+//         return i;
     }
 };
