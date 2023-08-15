c7fd89a41d276523337ee50118ccbe69e33f9ddc
Rishabh Kumar Pandey <118062111+rishabhpandey106@users.noreply.github.com>
2023-08-15 20:03:08 +0530

    Time: 43 ms (97.22%), Space: 42.3 MB (10.23%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const ans = [];
+    arr.forEach((element , index)=> {
+        if(fn(element , index))
+            {
+                ans.push(element);
+            }
+    })
+    return ans;
+};
